de95e1f5f10ee3b0b1831bdf44af36ea30678e75
Herwal <herwal20@gmail.com>
2023-10-31 19:37:18 +0100

    added days left of year

--- code/src/main.js
+++ code/src/main.js
@@ -3,12 +3,14 @@ import {  getSecondsLeftOfYear, getTimeString } from "./clock";
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
 const minutesLeft = document.getElementById("minutes-left");
+const daysLeft = document.getElementById("days-left");
 
 const render = () => {
   const now = new Date();
   timeContainer.innerText = getTimeString(now);
   timeLeft.innerText = getSecondsLeftOfYear(now);
   minutesLeft.innerText = (getSecondsLeftOfYear(now) / 60).toFixed(0);
+  daysLeft.innerText = (getSecondsLeftOfYear(now) / 3600 / 24).toFixed(0);
 }
 
 render();
