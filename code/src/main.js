92c82c706593a85293e5c7a84f0f3dd1637048c4
Herwal <herwal20@gmail.com>
2023-10-31 19:18:46 +0100

    last excercise! added minutes left

--- code/src/main.js
+++ code/src/main.js
@@ -2,11 +2,13 @@ import {  getSecondsLeftOfYear, getTimeString } from "./clock";
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
+const minutesLeft = document.getElementById("minutes-left");
 
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
   timeContainer.innerText = getTimeString(now);
+  minutesLeft.innerText = (getSecondsLeftOfYear(now) / 60).toFixed(2);
 }
 
 render();
