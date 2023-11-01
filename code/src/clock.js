1d49b3a3fce10524ef0064132c7f929d167eb099
Herwal <herwal20@gmail.com>
2023-11-01 11:05:05 +0100

    adda timer til helg, funka halveis

--- code/src/clock.js
+++ code/src/clock.js
@@ -11,9 +11,35 @@ const getNextYearDate = (now) => {
     return endOfYearDate;
 }
 
+const getCurrentDate = (now) => {
+    const currentDate = new Date(0);
+    currentDate.setUTCDate(now.getUTCDate());
+    return currentDate;
+}
+
+const getNextWeekendDate = (now) => {
+    const nextWeekendDate = new Date(now);
+    const dayOfWeek = nextWeekendDate.getUTCDay();
+  
+    // Calculate the number of days until the next Saturday (6 - dayOfWeek)
+    const daysUntilSaturday = (6 - dayOfWeek + 7) % 7;
+  
+    const timeUntilSaturday = daysUntilSaturday * 24 * 60  *60 * 1000;
+
+    nextWeekendDate.setTime(nextWeekendDate.getTime() + timeUntilSaturday);
+  
+    return nextWeekendDate;
+  };
+
 export const getSecondsLeftOfYear = (now) => {
     const startOfNextYear = getNextYearDate(now);
     const startOfNextYearMillis = startOfNextYear.getTime();
     const nowMillis = now.getTime();
     return Math.floor((startOfNextYearMillis - nowMillis) / 1000);
+}
+
+export const getDates = (now) => {
+    const currDate = getCurrentDate(now);
+    const nextWeekendDate = getNextWeekendDate(now);
+    return { currDate, nextWeekendDate };
 }
--- code/src/main.js
+++ code/src/main.js
@@ -1,9 +1,11 @@
-import {  getSecondsLeftOfYear, getTimeString } from "./clock";
+import {  getSecondsLeftOfYear, getTimeString, getDates} from "./clock";
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
 const minutesLeft = document.getElementById("minutes-left");
 const daysLeft = document.getElementById("days-left");
+const nextWeekend = document.getElementById("next-weekend");
+const currDate = document.getElementById("curr-date");
 
 const render = () => {
   const now = new Date();
@@ -11,6 +13,22 @@ const render = () => {
   timeLeft.innerText = getSecondsLeftOfYear(now);
   minutesLeft.innerText = (getSecondsLeftOfYear(now) / 60).toFixed(0);
   daysLeft.innerText = (getSecondsLeftOfYear(now) / 3600 / 24).toFixed(0);
+
+  const nextWeekendDate = getDates(now).nextWeekendDate;
+  const timeRemaining = nextWeekendDate.getTime() - now.getTime();
+
+  // Convert the time remaining to hours, minutes, and seconds
+  const hoursRemaining = Math.floor(timeRemaining / 3600000);
+  // const minutesRemaining = Math.floor((timeRemaining % 3600000) / 60000);
+  // const secondsRemaining = Math.floor((timeRemaining % 60000) / 1000);
+
+  nextWeekend.innerText = `${hoursRemaining}h`;
+
+  const day = now.getDate().toString().padStart(2, '0');
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const year = now.getFullYear().toString().padStart(2, '0');
+  const currentDate = `${day}.${month}.${year}`;
+  currDate.innerText = `${currentDate}`
 }
 
 render();
